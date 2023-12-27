cac8671f8e5dec5039cf51b70010df990a8e23ff
David-Albarracin <90213684+David-Albarracin@users.noreply.github.com>
2023-12-27 17:01:10 -0500

    .

---
+++ 100_Ejercicios_py/5.py
@@ -0,0 +1,19 @@
+"""
+Hacer un algoritmo que muestre la Serie Fibonacci. 0,1,1,2,3,5,8,13,21,34 
+
+Se trata de una secuencia infinita de números naturales; a partir del 0 y el 1, 
+se van sumando a pares, de manera que cada número es igual a la suma de sus dos anteriores, 
+"""
+
+#importamos el modulo de codigo reusable mas info en reusable.py
+import reusable
+
+a = 0
+b = 1
+c = 0
+
+for x in range(10):
+    print(f"{c}")
+    a = b
+    b = c
+    c = a + b
---
+++ 100_Ejercicios_py/6.py
@@ -0,0 +1,7 @@
+"""
+Algoritmo que muestre la siguiente progresión aritmética : 1,5,7,10,13,15,19,20,25,25
+"""
+
+#importamos el modulo de codigo reusable mas info en reusable.py
+import reusable
+
