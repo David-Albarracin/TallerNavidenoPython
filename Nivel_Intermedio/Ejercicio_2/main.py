20887682fe4ff79e0df3b23184cbf20ec45ab942
David-Albarracin <90213684+David-Albarracin@users.noreply.github.com>
2023-12-30 22:14:26 -0500

    Feliz Año

---
+++ Nivel_Intermedio/Ejercicio_2/main.py
@@ -0,0 +1,18 @@
+"""
+2. La alcaldía de Bucaramanga desea realizar un programa que le permita calcular el valor de CO2 producido
+En las diferentes instalaciones gubernamentales de la ciudad. Tenga en cuenta las siguientes observaciones:
+
+Para calcular el consumo de electricidad, sigue estos pasos:
+
+El programa debe permitir mostrar cual de las instalaciones tiene mayor producción de CO2.
+Requerimientos: El programa debe contar con un menú principal que permita realizar todos los
+Procesos solicitados.
+
+1. Registrar Dependencia
+2. Registrar consumo por dependencia : Tengan en cuenta que se debe registrar los valores
+consumidos por los dispositivos en cada una de las oficinas.
+3. Ver CO2 producido
+4. Dependencia que produce mayor CO2
+5. Salir
+
+"""
